c93b3c263e6d874b6dfadd24d0b63a5bf2e9729a
Anna Gaughan <agaughan@gmail.com>
2015-05-26 18:51:00 +0100

    mortgage calculator working

--- main.rb
+++ main.rb
@@ -43,10 +43,21 @@ end
       @height = params[:height].to_f
       @result = @weight/(@height * @height).to_f
     end  
-  
   erb :bmi
   end  
 
+  get '/mortgage' do
+    @principal = params[:principal].to_f 
+    @interest = params[:interest].to_f
+    @months = params[:months].to_i
+    
+       # @result_mort_calc = (@principal * (@interest * (1 + @interest)**@months) / ((1+@interest)**@months - 1)).round(2)
+
+    @result = (@principal * (@interest * (1 + @interest)**@months) / ((1+@interest)**@months - 1)).round(2)
+      # @result =  @principal(@interest(1+@interest)*@months)/((1+@interest)*@months -1)
+    # end
+    erb :mortgage
+  end  
   # Your BMI is #{weight_m/(height_m**2)}"
 
 
